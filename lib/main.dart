0e3f503a8d136a3fb0b50c30140b60b90c0972cf
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2024-06-28 17:34:45 -0300

    Workmanager configurations ; Tested on a physical Android device

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:background_tasks_flutter_playground/src/app_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:workmanager/workmanager.dart';
 
-void main() {
-  runApp(const AppWidget());
+@pragma(
+    'vm:entry-point') // Mandatory if the App is obfuscated or using Flutter 3.1+
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) {
+    print(
+        "Native called background task: $task"); //simpleTask will be emitted here.
+    return Future.value(true);
+  });
 }
 
-
+void main() {
+  Workmanager().initialize(
+      callbackDispatcher, // The top level function, aka callbackDispatcher
+      isInDebugMode:
+          true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
+      );
+  Workmanager().registerOneOffTask("task-identifier", "simpleTask");
+  runApp(const AppWidget());
+}
